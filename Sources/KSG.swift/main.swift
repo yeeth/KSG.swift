2483707512cda50d89df7f188895a6163a352213
decanus <dean.eigenmann@icloud.com>
2019-01-20 04:51:51 +0100

    updated

--- Sources/KSG.swift/main.swift
+++ Sources/KSG.swift/main.swift
@@ -5,17 +5,20 @@ let g = EthereumResearchGhost()
 let startTime = CFAbsoluteTimeGetCurrent()
 for i in stride(from: 0, to: 131072, by: 1024) {
     let head = g.head()
-    for _ in i...(i + g.NODE_COUNT) {
+    var phead: Data = Data(capacity: 32)
+    for _ in stride(from: i, to: i + 1024, by: 1) {
         let phead = g.getPerturbedHead(h: head)
         g.addAttestations(block: phead, v: i % g.NODE_COUNT)
-        let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
-        print(
-            NSString(
-                format: "Adding new block on top of block %d, Time: %.5f",
-                (g.blocks[phead]?.0 ?? 0),
-                timeElapsed
-            )
-        )
-        g.addBlock(parent: phead)
     }
+
+    let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+    print(
+        NSString(
+            format: "Adding new block on top of block %d, Time: %.5f",
+            (g.blocks[phead]?.0 ?? 0),
+            timeElapsed
+        )
+    )
+
+    g.addBlock(parent: phead)
 }
