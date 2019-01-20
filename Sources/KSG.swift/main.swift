ee4f1ca32adc955c15e6f9593c27fdb7b87de226
decanus <dean.eigenmann@icloud.com>
2019-01-20 05:37:13 +0100

    fixed

--- Sources/KSG.swift/main.swift
+++ Sources/KSG.swift/main.swift
@@ -3,11 +3,11 @@ import Foundation
 let g = EthereumResearchGhost()
 
 let startTime = CFAbsoluteTimeGetCurrent()
+var phead: Data = Data(capacity: 32)
 for i in stride(from: 0, to: 131072, by: 1024) {
     let head = g.head()
-    var phead: Data = Data(capacity: 32)
     for _ in stride(from: i, to: i + 1024, by: 1) {
-        let phead = g.getPerturbedHead(h: head)
+        phead = g.getPerturbedHead(h: head)
         g.addAttestations(block: phead, v: i % g.NODE_COUNT)
     }
 
