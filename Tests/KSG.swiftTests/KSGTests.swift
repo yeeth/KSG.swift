6ebec196c97257a062224c7f06aee56b9b917ecc
decanus <dean.eigenmann@icloud.com>
2019-01-21 03:24:48 +0100

    updated package

--- Tests/KSG.swiftTests/KSGTests.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import KSG_swift
-
-final class KSG_swiftTests: XCTestCase {
-
-}
---
+++ Tests/KSGTests/KSGTests.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import KSG
+
+final class KSGTests: XCTestCase {
+
+}
